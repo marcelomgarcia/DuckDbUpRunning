6dfa7e3864e09347ad7cd8e8bd13fdaa7b8de0ed
Marcelo Garcia <marcelomgarcia@gmail.com>
2025-08-08 14:38:41 +0300

    initial commit

---
+++ DuckDbUpRunning.py
@@ -0,0 +1,20 @@
+import marimo
+
+__generated_with = "0.14.16"
+app = marimo.App()
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import duckdb
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
