0b0096c191060d5ffb096b78a3d18a66e2af9588
Marcelo Garcia <marcelomgarcia@gmail.com>
2026-01-16 12:55:43 +0300

    Update

--- DuckDbUpRunning.py
+++ DuckDbUpRunning.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.19.2"
+__generated_with = "0.19.4"
 app = marimo.App()
 
 
@@ -9,11 +9,28 @@ def _():
     import marimo as mo
     import duckdb
     import polars as pl
+    return duckdb, mo
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    Learning DuckDB
+    ===============
+
+    The dataset is [Public Sector Network](https://www.kaggle.com/datasets/mirzamilanfarabi/public-sector-network) because it seems to be enough complex with a few files and still small enough to be uploaded to GitHub.
+    """)
     return
 
 
 @app.cell
-def _():
+def _(duckdb):
+    conn = duckdb.connect()
+    query = '''
+      SELECT Node_ID,Type,Region 
+      FROM read_csv_auto('data/nodes.csv')
+      '''
+    df = conn.execute(query).df()
     return
 
 
